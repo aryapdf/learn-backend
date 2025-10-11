2bea13e5eacd9d9b4baf6392205b5332b917f7aa
arya-sgt <aryaagnipradana@gmail.com>
2025-10-12 01:50:25 +0700

    update condition and proper error handling

--- index.js
+++ index.js
@@ -9,25 +9,68 @@ const rl = readline.createInterface({
 const [,, command, ...args] = process.argv;
 
 async function callEvents(user) {
-    const response = await fetch(`https://api.github.com/users/${user}/events`);
-    const data =  await response.json();
-    return data.map((eachData) => ({
-        id: eachData.id,
-        type: eachData.type,
-        name: eachData.actor.display_login,
-        repo_name: eachData.repo.name,
-    }));
+    try {
+        const response = await fetch(`https://api.github.com/users/${user}/events`);
+
+        if (!response.ok) {
+            if (response.status === 404) {
+               return 'USER_NOT_FOUND'
+            } else {
+                return `${response.status} ${response.statusText}`
+            }
+        }
+
+
+        const data =  await response.json();
+
+        if (data.length === 0) {
+            return 'NO_ACTIVITY'
+        }
+        return data.map((eachData) => ({
+            id: eachData.id,
+            type: eachData.type,
+            name: eachData.actor.display_login,
+            repo_name: eachData.repo.name,
+        }));
+    } catch(e) {
+        return `${e.code} ${e.message}`
+    }
+
+
 }
 
-rl.question('Hello, Who are you? ', (val) => {
+rl.question('Hello, Who are you? ', async (val) => {
+    if (!val.trim()) {
+        console.log(`You didn't input the user correctly. Try again.`)
+        rl.close();
+    }
+
     console.log(`Hello ${val}!`);
-    console.log(`Fetching your data...`);
-    callEvents(val).then((res) => {
-        if (res.length === 0) {
-            console.log(`The user you look is not available! Try again?`);
+    console.log(`Fetching your data... \n`);
+
+    try {
+        const response = await callEvents(val);
+        if (typeof response !== 'object') {
+            switch (response) {
+                case 'USER_NOT_FOUND':
+                    console.log("User not found.. did you insert the correct username?")
+                    console.log("Try again.")
+                    break;
+                case 'NO_ACTIVITY':
+                    console.log("It seems this user not active for a while...")
+                    console.log("No activity recorded.")
+                    break;
+                default:
+                    console.log('Something went wrong...');
+                    console.log(`Error: ${e}`);
+            }
         } else {
-            console.log(`Github events data : `, res)
+            console.log('Github User Activity')
+            console.table(response)
         }
-    });
-    rl.close();
+    } catch (e) {
+        throw new Error(e)
+    } finally {
+        rl.close();
+    }
 })

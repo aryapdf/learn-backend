2206d7c6656fa681e99d7c9d5e6ce1af3dbe152e
arya-sgt <aryaagnipradana@gmail.com>
2025-10-12 01:18:57 +0700

    fetch github events

--- index.js
+++ index.js
@@ -8,7 +8,26 @@ const rl = readline.createInterface({
 
 const [,, command, ...args] = process.argv;
 
+async function callEvents(user) {
+    const response = await fetch(`https://api.github.com/users/${user}/events`);
+    const data =  await response.json();
+    return data.map((eachData) => ({
+        id: eachData.id,
+        type: eachData.type,
+        name: eachData.actor.display_login,
+        repo_name: eachData.repo.name,
+    }));
+}
+
 rl.question('Hello, Who are you? ', (val) => {
     console.log(`Hello ${val}!`);
+    console.log(`Fetching your data...`);
+    callEvents(val).then((res) => {
+        if (res.length === 0) {
+            console.log(`The user you look is not available! Try again?`);
+        } else {
+            console.log(`Github events data : `, res)
+        }
+    });
     rl.close();
 })
